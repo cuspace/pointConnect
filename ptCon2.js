500f20b1863e3611db984448e016dc1f12228126
cuspace <cuspace@gmail.com>
2016-03-11 11:01:36 +0900

    추가: Point.create 점들의 좌표 넣고 테스트

--- ptCon2.js
+++ ptCon2.js
@@ -4,15 +4,40 @@
 	
 	var Point = (function() {
 		var _sceneEl;
+		var	_template = {
+			id: 'point',
+			geometry: {
+				primitive: 'box',
+				depth: 0.2,
+				width: 0.5,
+				height: 0.5
+			},
+			material: {
+				color: 'gray',
+				metalness: 0.5
+			}
+		};
 		var create = function (sceneId, pos2dArr) {
 			_sceneEl = document.getElementById(sceneId);
-			console.log(_sceneEl);
+			pos2dArr.forEach(function (item, index, array) {
+				var aEntity = document.createElement('a-entity');
+				aEntity.setAttribute('id', _template.id + index);
+				aEntity.setAttribute('geometry', _template.geometry);
+				aEntity.setAttribute('material', _template.material);
+				aEntity.setAttribute('position', item);
+				_sceneEl.appendChild(aEntity);
+			});
 		};
 		return {
 			create: create
 		};
 	})();
 	
-	Point.create('scene1');
+	Point.create('scene1', [
+		{x: -0.5, y: +1.0, z: -5.0},
+		{x: +0.5, y: +1.0, z: -5.0},
+		{x: -2.0, y: -1.0, z: -5.0},
+		{x: +2.0, y: -1.0, z: -5.0}
+	]);
 
 })();
