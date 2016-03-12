010e74a0435243b627675a20c5f8a0d9c0fad4e7
cuspace <cuspace@gmail.com>
2016-03-12 13:00:08 +0900

    추가: 모든 선 그리기 완료

--- ptCon2.js
+++ ptCon2.js
@@ -1,4 +1,5 @@
 /*jshint browser: true, devel: true*/
+/*globals vec3*/
 (function() {
 	"use strict";
 	
@@ -57,15 +58,43 @@
 				}
 			}
 		};
+		var _getMidPos = function (p1, p2) {
+			return {x: (p1.x+p2.x)*0.5, y: (p1.y+p2.y)*0.5, z: (p2.z+p2.z)*0.5};
+		};
+		var _getHeight = function (p1, p2) {
+			return vec3.len(vec3.fromValues(p1.x-p2.x, p1.y-p2.y, p1.z-p2.z));
+		};
+		var _getRotation = function (p1, p2) {
+			var ref = vec3.fromValues(0, 1, 0);
+			var v1 = vec3.fromValues(p1.x-p2.x, p1.y-p2.y, p1.z-p2.z);
+			var angle = vec3.angle(v1, ref)*180/Math.PI;
+			angle = (p1.x < p2.x) ? angle : -angle;
+			return {x:0,y:0,z:angle};
+		};
 		var create = function (sceneId, positionsArr) {
 			_makePairTbl(positionsArr.length);
+			
 			_sceneEl = document.getElementById(sceneId);
-			var aEntity = document.createElement('a-entity');
-			aEntity.setAttribute('id', _template.id);
-			aEntity.setAttribute('geometry', _template.geometry);
-			aEntity.setAttribute('material', _template.material);
-			aEntity.setAttribute('visible', _template.visible);
-			_sceneEl.appendChild(aEntity);
+			
+			for(var i = 0; i < _pairTbl.length; i++) {
+				for(var j = i+1; j < _pairTbl[i].length; j++) {
+					
+					var pos = _getMidPos(positionsArr[i], positionsArr[j]);
+					var h = _getHeight(positionsArr[i], positionsArr[j]);
+					var rot = _getRotation(positionsArr[i], positionsArr[j]);
+					console.log('p1.x:'+positionsArr[i].x+', p2.x:'+positionsArr[j].x+' , rot:'+rot.z);
+					
+					var aEntity = document.createElement('a-entity');
+					aEntity.setAttribute('id', _template.id);
+					aEntity.setAttribute('geometry', _template.geometry);
+					aEntity.setAttribute('geometry', 'height', h);
+					aEntity.setAttribute('material', _template.material);
+					aEntity.setAttribute('visible', _template.visible);
+					aEntity.setAttribute('position', pos);
+					aEntity.setAttribute('rotation', rot);
+					_sceneEl.appendChild(aEntity);
+				}
+			}
 		};
 		return {
 			create: create
